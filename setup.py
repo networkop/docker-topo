ec0496c9c6e697f460ad5d15abbd1260b66a79d9
networkop <mmkashin@gmail.com>
2018-02-16 11:09:35 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='arista-ceoslab',
+    version='1.0',
+    scripts=['topo-ceos'],
+    install_requires=[
+        'argparse',
+        'logging',
+        'yaml',
+        'docker'
+    ],
+    url='https://github.com/networkop/arista-ceos-topo',
+    license='BSD3',
+    author='Michael Kashin',
+    author_email='mkashin@aristsa.com',
+    description='Arista cEOS topology builder'
+)
