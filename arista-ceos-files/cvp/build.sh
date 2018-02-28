c3068a2b570f79e0c5408aa9e3496636b3321f40
networkop <mmkashin@gmail.com>
2018-02-28 18:49:09 +0000

    more bug fixes

--- arista-ceos-files/cvp/build.sh
+++ arista-ceos-files/cvp/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+CVP_PATH=$1
+TOOLS_PATH=$2
+ANSWER_PATH=$3
+
 check_if_file_exists () {
   if [ ! -f $1 ]; then
     echo "$1 file not found!"
@@ -7,12 +11,22 @@ check_if_file_exists () {
   fi
 }
 
+copy_to_cwd () {
+  if [ ! -f $2 ]; then
+    cp -n $1 $2
+  fi
+}
+
+copy_to_cwd $CVP_PATH cvp.tgz
+copy_to_cwd $TOOLS_PATH cvp-tools.tgz
+copy_to_cwd $ANSWER_PATH answers.yaml
+
 check_if_file_exists cvp.tgz
-
 check_if_file_exists cvp-tools.tgz
-
 check_if_file_exists answers.yaml
 
 chmod 660 /dev/kvm && chown root:qemu /dev/kvm
 
-docker build -t cvp .
+CMD="docker build -t cvp ."
+echo $CMD
+eval $CMD
