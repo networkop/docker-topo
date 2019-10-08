e10fc71bc07196b0b78c5fec88de31565167c63d
networkop <mmkashin@gmail.com>
2019-10-08 22:06:07 +0100

    Added support for latest ceos versions

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ def collect_files():
 
 setup(
     name="docker-topo",
-    version="2.0.2",
+    version="2.0.3",
     scripts=["bin/docker-topo"],
     data_files=collect_files(),
     python_requires=">=3.5",
-    install_requires=["pyyaml", "docker", "netaddr"] + (["pyroute2==0.5.3"] if 'linux' in sys.platform.lower() else []),
+    install_requires=["pyyaml", "docker", "netaddr", "packaging"] + (["pyroute2==0.5.3"] if 'linux' in sys.platform.lower() else []),
     url="https://github.com/networkop/docker-topo",
     license="BSD3",
     author="Michael Kashin",
